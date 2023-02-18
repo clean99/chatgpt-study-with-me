6e8d36e0f87348e1eed37fb2fdba2d36b184ff34
feng.xu <feng.xu@bytedance.com>
2023-02-18 13:55:21 +0800

    feat: init with auth

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,46 @@
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  DynamicModule,
+} from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { AuthMiddleware } from './auth.middleware';
+import { ConfigInjectionToken, AuthModuleConfig } from './config.interface';
+import { SupertokensService } from './supertokens/supertokens.service';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  providers: [SupertokensService],
+  exports: [],
+  controllers: [],
+})
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('*');
+  }
+
+  static forRoot({
+    connectionURI,
+    apiKey,
+    appInfo,
+  }: AuthModuleConfig): DynamicModule {
+    return {
+      providers: [
+        {
+          useValue: {
+            appInfo,
+            connectionURI,
+            apiKey,
+          },
+          provide: ConfigInjectionToken,
+        },
+        SupertokensService,
+      ],
+      exports: [],
+      imports: [],
+      module: AuthModule,
+    };
+  }
+}
---
+++ frontend/src/config.tsx
@@ -0,0 +1,25 @@
+import ThirdPartyPasswordless from "supertokens-auth-react/recipe/thirdpartypasswordless";
+import Session from "supertokens-auth-react/recipe/session";
+console.log("process.env.REACT_APP_API_DOMAIN", process.env.REACT_APP_API_DOMAIN);
+export const SuperTokensConfig = {
+    appInfo: {
+        appName: "Studywithme",
+        apiDomain: process.env.REACT_APP_API_DOMAIN ?? '',
+        websiteDomain: process.env.REACT_APP_WEBSITE_DOMAIN ?? '',
+    },
+    // recipeList contains all the modules that you want to
+    // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
+    recipeList: [
+        ThirdPartyPasswordless.init({
+            signInUpFeature: {
+                providers: [
+                    ThirdPartyPasswordless.Github.init(),
+                    ThirdPartyPasswordless.Google.init(),
+                    ThirdPartyPasswordless.Apple.init(),
+                ],
+            },
+            contactMethod: "EMAIL_OR_PHONE",
+        }),
+        Session.init(),
+    ],
+};
