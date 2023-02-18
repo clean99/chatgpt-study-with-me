9aee4711201c13a4bfe3cf3f2a76d6d339dad64b
feng.xu <feng.xu@bytedance.com>
2023-02-18 14:24:35 +0800

    feat: update env path

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config();
+dotenv.config({ path: '.env.local' });
 import { AppModule } from './app.module';
 import supertokens from 'supertokens-node';
 import { SupertokensExceptionFilter } from './auth/auth.filter';
